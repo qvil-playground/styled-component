5cf46261eab906b71aefcceb3037007235701f2e
qvil <qvil1127@gmail.com>
2018-05-11 22:40:11 +0900

    Old version button using css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import "./App.css";
+
+const Button = ({ danger, text }) => {
+  return (
+    <button
+      className={danger ? "button button--danger" : "button button--success"}
+    >
+      {text || "Button"}
+    </button>
+  );
+};
 
 class App extends Component {
   render() {
-    return <div className="App">App</div>;
+    return (
+      <Fragment>
+        <Button />
+        <Button danger text={"Danger"} />
+      </Fragment>
+    );
   }
 }
 
